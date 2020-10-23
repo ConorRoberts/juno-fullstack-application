fbd2b1875e0a9017560f6da083a73bdb16e9c465
ConorRoberts <62777912+ConorRoberts@users.noreply.github.com>
2020-10-23 17:38:21 -0400

    Fixed sorting issue

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
-import React, { Component,useState } from "react";
+import React, { Component, useState } from "react";
 import tiffImage from "./tiff.png";
+import filmLogo from "./noun_Movie_1241202.svg";
 
 function App() {
   return (
@@ -12,36 +13,50 @@ function App() {
 }
 
 function Film(props) {
-  const [isOpen,setOpen]=useState(false);
+  const [isOpen, setOpen] = useState(false);
 
-  const genreStrings = props.genres.map(genre => genre.name);
+  const genreStrings = props.genres.map((genre) => genre.name);
 
   // console.log(props.obj);
   return (
-    <div onClick={()=>setOpen(!isOpen)} className="film-container">
-      <h1>
-        {props.title}
-      </h1>
-      <h1>
-        {props.release}
-      </h1>
-       {isOpen && 
-       <div className="film-properties">
-        <p>{props.overview}</p>
-        <p>Genres: {genreStrings.join(", ")}</p>
-        <p>Runtime: {props.runTime}</p>
-        {props.tagline && <p>Tagline: {props.tagline}</p>}
-      </div>}
+    <div onClick={() => setOpen(!isOpen)} className="film-container">
+      <div className="title-container">
+        <img className="film-reel" src={filmLogo} alt="Film reel icon" />
+        <h1>
+          {props.title}
+          <br />
+          {props.release}
+        </h1>
+      </div>
+      {isOpen && (
+        <div className="film-properties">
+          <p>{props.overview}</p>
+          <p>Genres: {genreStrings.join(", ")}</p>
+          <p>Runtime: {props.runTime}</p>
+          {props.tagline && <p>Tagline: {props.tagline}</p>}
+        </div>
+      )}
     </div>
   );
+}
+
+function compareDates(a, b) {
+  if (a < b) {
+    return -1;
+  }
+
+  if (a > b) {
+    return 1;
+  }
+
+  return 0;
 }
 
 class FilmList extends Component {
   constructor(props) {
     super();
     this.state = {
-      data: [],
-      movies:[]
+      movies: [],
     };
   }
 
@@ -60,46 +75,29 @@ class FilmList extends Component {
         // Grab single page result, convert to json
         const pageResult = await fetch(
           `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=movie&include_adult=true&primary_release_year=2020&page=${i}`
+        );
+        const pageData = await pageResult.json();
+
+        // Getting detailed movie data for each movie on the page
+        pageData.results.forEach(async (movie) => {
+          // Array to store movie details so they can update final state
+          let arr = [];
+
+          // Request
+          const movieResult = await fetch(
+            `https://api.themoviedb.org/3/movie/${movie.id}?api_key=${key}`
           );
-          const pageData = await pageResult.json();
-          
-          // Getting detailed movie data for each movie on the page
-          pageData.results.forEach(async(movie)=>{
-            // Array to store movie details so they can update final state
-            let arr=[]
+          const movieData = await movieResult.json();
 
-            // Request
-            const movieResult = await fetch(`https://api.themoviedb.org/3/movie/${movie.id}?api_key=${key}`);
-            const movieData = await movieResult.json();
-
+          if (movie.popularity >= 10) {
             // Combining previous array and current arr details to update state
             arr = this.state.movies.concat(movieData);
 
             // Update state
-            this.setState({movies:arr});
+            this.setState({ movies: arr });
+          }
         });
       }
-
-      function compareDates(a,b){
-        if (a<b){
-          return -1;
-        }
-      
-        if (a>b){
-          return 1;
-        }
-      
-        return 0;
-      }
-
-      // Grabbing current movie list
-      const arr = this.state.movies;
-
-      // Sorting list with above compare function
-      arr.sort((a,b) => compareDates(new Date(a.release_date),new Date(b.release_date)));
-
-      // Set new state of movies array
-      this.setState({ movies: arr });
     };
 
     // Calls function to update data state
@@ -108,20 +106,21 @@ class FilmList extends Component {
   render() {
     return (
       <div className="film-list">
-        {this.state.movies.map(
-          (movie) =>
-            movie.popularity >= 10 && (
-              <Film
-                overview={movie.overview}
-                title={movie.title}
-                genres={movie.genres}
-                release={movie.release_date}
-                obj={movie}
-                runTime={movie.runtime}
-                tagline={movie.tagline}
-              />
-            )
-        )}
+        {this.state.movies
+          .sort((a, b) =>
+            compareDates(new Date(a.release_date), new Date(b.release_date))
+          )
+          .map((movie) => (
+            <Film
+              overview={movie.overview}
+              title={movie.title}
+              genres={movie.genres}
+              release={movie.release_date}
+              obj={movie}
+              runTime={movie.runtime}
+              tagline={movie.tagline}
+            />
+          ))}
       </div>
     );
   }
